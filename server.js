57687775c445037871889a7d8079b7a5f66571d7
Hack Reactor Students <accounts@hackreactor.com>
2015-04-25 16:17:05 -0700

    Refactor server code to use mongoose and mongodb.

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
 var app = require('./server-config.js');
 
-// var port = process.env.PORT || 4568;
+var port = process.env.PORT || 4568;
 
-// app.listen(port);
+app.listen(port);
 
-// console.log('Server now listening on port ' + port);
+console.log('Server now listening on port ' + port);
 
 /////////////////////////////////
 // Mongo implementation
 /////////////////////////////////
 
 // var express     = require('express'),
-var mongoose = require('mongoose');
 
-// var app = express();
-
-mongoose.connect('mongodb://localhost/shortly'); // connect to mongo database named shortly
 
 // configure our server with all the middleware and and routing
-require('./config/middleware.js')(app, express);
+// require('./config/middleware.js')(app, express);
 
 // export our app for testing and flexibility, required by index.js
-module.exports = app;
+// module.exports = app;
 
